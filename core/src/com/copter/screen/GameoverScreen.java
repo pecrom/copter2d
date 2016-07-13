229be789ec7ae5b6d5dd8fcf0b1d93c12e19c601
Roman Pecek <roman.pecek@schneider-electric.com>
2016-07-13 20:43:27 +0200

    Temporary game over screen

--- core/src/com/copter/screen/GameoverScreen.java
+++ core/src/com/copter/screen/GameoverScreen.java
@@ -1,7 +1,23 @@
 package com.copter.screen;
 
 import com.badlogic.gdx.ScreenAdapter;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class GameoverScreen extends ScreenAdapter {
+  private SpriteBatch batch;
+  private BitmapFont font;
+  
+  public GameoverScreen() {
+    batch = new SpriteBatch();
+    font = new BitmapFont();
+  }
+  
+  @Override
+  public void render(float delta) {
+    batch.begin();
+    font.draw(batch, "Game over", 350, 200);
+    batch.end();
+  }
 
 }
